3296327ccd723c33393fd505e0d31f511765f0e7
ief44 <1213842@student.roc-nijmegen.nl>
2026-05-28 11:58:59 +0200

    locationReached aangemaakt, getest en hij is werkend

--- MyDodo.java
+++ MyDodo.java
@@ -214,11 +214,14 @@ public class MyDodo extends Dodo
           }
       }
   }
-  public void faceEast() {
+  public void faceEast() { // hij draait ongeacht op welke positie die zit draait die naar het oosten
       while (getDirection() != EAST) {
           turnRight();
       }
   }
+  public boolean locationReached(int x, int y) {
+      return getX() == x && getY() == y;
+  }  //check of de dodo al op de coordinaten juiste coordinaten staat.
 }
 
    
