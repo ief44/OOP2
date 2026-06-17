5edcf3ec503fdd0c64f4b0521687794f6300c3f0
ief44 <1213842@student.roc-nijmegen.nl>
2026-06-17 09:22:28 +0200

    SupriseEgg toegevoegt en arraylist gemaakt voor makeListofSupriseEgg

---
+++ __SHELL0.java
@@ -0,0 +1,10 @@
+
+public class __SHELL0 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\Ief44\\OneDrive - ROC Nijmegen\\School\\DodoScenario1");
+Mauritius mauritiu1 = (Mauritius)__bluej_runtime_scope.get("mauritiu1");
+
+
+mauritiu1.populateFromFile();
+
+}}
